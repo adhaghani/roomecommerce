e8c266be7e71ec503da66e1168625df698f099de
adhaghani <adhaahmad04@gmail.com>
2024-07-02 10:47:06 +0800

    modified backend

--- BackEnd/CSC264/RoomAPI/RegisterUser.php
+++ BackEnd/CSC264/RoomAPI/RegisterUser.php
@@ -13,29 +13,42 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method === 'POST') {
     $products = json_decode(file_get_contents("php://input"));
-    $sql = "INSERT INTO useraccount(Username, Password, RoleID, firstName, lastName, emailAddress,phoneNumber,addressLine1,addressLine2,postCode,city,country,dateRegistered)
-                      VALUES (:Username,:Password,:RoleID,:firstName,:lastName,:emailAddress,:phoneNumber,:addressLine1,:addressLine2,:postCode,:city,:country,:dateRegistered)";
-    $stmt = $conn->prepare($sql);
-    $register_at = date('Y-m-d');
-    $stmt->bindParam(':Username', $products->Username);
-    $hashedpassword = password_hash($products->Password, PASSWORD_DEFAULT);
-    $stmt->bindParam(':Password', $hashedpassword);
-    $stmt->bindParam(':RoleID', $products->RoleID);
-    $stmt->bindParam(':firstName', $products->firstName);
-    $stmt->bindParam(':lastName', $products->lastName);
-    $stmt->bindParam(':emailAddress', $products->emailAddress);
-    $stmt->bindParam(':phoneNumber', $products->phoneNumber);
-    $stmt->bindParam(':addressLine1', $products->addressLine1);
-    $stmt->bindParam(':addressLine2', $products->addressLine2);
-    $stmt->bindParam(':postCode', $products->postCode);
-    $stmt->bindParam(':city', $products->city);
-    $stmt->bindParam(':country', $products->country);
-    $stmt->bindParam(':dateRegistered', $register_at);
 
-    if ($stmt->execute()) {
-        $response = ['status' => 1, 'message' => 'Record created successfully.'];
+    // Check if username or email exists
+    $sql = "SELECT * FROM useraccount WHERE Username = :Username OR emailAddress = :emailAddress";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':Username', $products->Username);
+    $stmt->bindParam(':emailAddress', $products->emailAddress);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($result){
+        $response = ['status' => 0, 'message' => 'Account with the same username / email address already exists.'];
     } else {
-        $response = ['status' => 0, 'message' => 'Failed to create record.'];
+        $sql = "INSERT INTO useraccount(Username, Password, RoleID, firstName, lastName, emailAddress,phoneNumber,addressLine1,addressLine2,postCode,city,country,dateRegistered)
+                VALUES (:Username,:Password,:RoleID,:firstName,:lastName,:emailAddress,:phoneNumber,:addressLine1,:addressLine2,:postCode,:city,:country,:dateRegistered)";
+        $stmt = $conn->prepare($sql);
+        $register_at = date('Y-m-d');
+        $stmt->bindParam(':Username', $products->Username);
+        $hashedpassword = password_hash($products->Password, PASSWORD_DEFAULT);
+        $stmt->bindParam(':Password', $hashedpassword);
+        $stmt->bindParam(':RoleID', $products->RoleID);
+        $stmt->bindParam(':firstName', $products->firstName);
+        $stmt->bindParam(':lastName', $products->lastName);
+        $stmt->bindParam(':emailAddress', $products->emailAddress);
+        $stmt->bindParam(':phoneNumber', $products->phoneNumber);
+        $stmt->bindParam(':addressLine1', $products->addressLine1);
+        $stmt->bindParam(':addressLine2', $products->addressLine2);
+        $stmt->bindParam(':postCode', $products->postCode);
+        $stmt->bindParam(':city', $products->city);
+        $stmt->bindParam(':country', $products->country);
+        $stmt->bindParam(':dateRegistered', $register_at);
+
+        if ($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record created successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to create record.'];
+        }
     }
 
     echo json_encode($response);
--- BackEnd/CSC264/RoomAPI/UpdateUser.php
+++ BackEnd/CSC264/RoomAPI/UpdateUser.php
@@ -14,25 +14,40 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method === 'POST') {
     $Product = json_decode(file_get_contents("php://input"));
-    $sql = "UPDATE useraccount SET firstName = :firstName, lastName = :lastName, emailAddress = :emailAddress, phoneNumber = :phoneNumber, addressLine1 = :addressLine1, addressLine2 = :addressLine2, postCode = :postCode, city = :city, country = :country, Username = :Username WHERE UserID = :UserID";
+
+    // Check if username or email exists
+    $sql = "SELECT * FROM useraccount WHERE UserID <> :UserID AND (Username = :Username OR emailAddress = :emailAddress)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':UserID', $Product->UserID);
-    $stmt->bindParam(':firstName', $Product->firstName);
-    $stmt->bindParam(':lastName', $Product->lastName);
-    $stmt->bindParam(':emailAddress', $Product->emailAddress);
-    $stmt->bindParam(':phoneNumber', $Product->phoneNumber);
-    $stmt->bindParam(':addressLine1', $Product->addressLine1);
-    $stmt->bindParam(':addressLine2', $Product->addressLine2);
-    $stmt->bindParam(':postCode', $Product->postCode);
-    $stmt->bindParam(':city', $Product->city);
-    $stmt->bindParam(':country', $Product->country);
     $stmt->bindParam(':Username', $Product->Username);
+    $stmt->bindParam(':emailAddress', $Product->emailAddress);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if($stmt->execute()){
-        $response = ['status' => 1, 'message' => 'Record Updated successfully.'];
-    }else{
-        $response = ['status' => 0, 'message' => 'Failed to Update record.'];
+    if($result){
+        $response = ['status' => 0, 'message' => 'Username or email already exists.'];
+    } else {
+        $sql = "UPDATE useraccount SET firstName = :firstName, lastName = :lastName, emailAddress = :emailAddress, phoneNumber = :phoneNumber, addressLine1 = :addressLine1, addressLine2 = :addressLine2, postCode = :postCode, city = :city, country = :country, Username = :Username WHERE UserID = :UserID";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':UserID', $Product->UserID);
+        $stmt->bindParam(':firstName', $Product->firstName);
+        $stmt->bindParam(':lastName', $Product->lastName);
+        $stmt->bindParam(':emailAddress', $Product->emailAddress);
+        $stmt->bindParam(':phoneNumber', $Product->phoneNumber);
+        $stmt->bindParam(':addressLine1', $Product->addressLine1);
+        $stmt->bindParam(':addressLine2', $Product->addressLine2);
+        $stmt->bindParam(':postCode', $Product->postCode);
+        $stmt->bindParam(':city', $Product->city);
+        $stmt->bindParam(':country', $Product->country);
+        $stmt->bindParam(':Username', $Product->Username);
+
+        if($stmt->execute()){
+            $response = ['status' => 1, 'message' => 'Record Updated successfully.'];
+        }else{
+            $response = ['status' => 0, 'message' => 'Failed to Update record.'];
+        }
     }
+
     echo json_encode($response);
     exit();
 }
